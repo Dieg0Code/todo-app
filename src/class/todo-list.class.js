a413257dcbc22b6557cc297ebe30ad7663320577
Dieg0Code <diegoobando20@gmail.com>
2022-03-03 04:33:55 -0300

    guardar y recuperar datos del localStorage

--- src/class/todo-list.class.js
+++ src/class/todo-list.class.js
@@ -3,16 +3,19 @@
 export class TodoList {
 
     constructor() {
-        this.todo = [];
+        // this.todo = [];
+        this.cargarLocalStorage();
     }
 
     nuevoTodo( todo ) {
         this.todo.push( todo );
+        this.guardarLocalStorage();
     }
 
     eliminarTodo( id ) {
 
         this.todo = this.todo.filter( todo => todo.id != id );
+        this.guardarLocalStorage();
 
     }
 
@@ -22,6 +25,7 @@ export class TodoList {
 
             if ( todo.id == id ) {
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
             }
         }
@@ -31,6 +35,19 @@ export class TodoList {
     eliminarCompletados() {
         
         this.todo = this.todo.filter( todo => !todo.completado );
+        this.guardarLocalStorage();
+
+    }
+
+    guardarLocalStorage() {
+
+        localStorage.setItem('todo',  JSON.stringify(this.todo));
+
+    }
+
+    cargarLocalStorage() {
+
+        this.todo = (localStorage.getItem('todo')) ? JSON.parse( localStorage.getItem('todo') ) : [];
 
     }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript');
-todoList.nuevoTodo(tarea);
-
-// tarea.completado = true;
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
+todoList.todo.forEach( todo => crearTodoHtml( todo ) );
